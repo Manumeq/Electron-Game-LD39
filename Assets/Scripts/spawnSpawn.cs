787be74f25da98765f4b1492a4c81d705c1aa049
Unknown <babioca7@hotmail.com>
2017-07-29 10:27:25 +0200

    Creado spawn point de spawn points

--- Assets/Scripts/spawnSpawn.cs
+++ Assets/Scripts/spawnSpawn.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class spawnSpawn : MonoBehaviour {
     private bool mov;
+    public GameObject spawnPoint;
 	// Use this for initialization
 	void Start () {
         mov = true;
@@ -11,9 +12,14 @@ public class spawnSpawn : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (mov)
+        {
+            Instantiate(spawnPoint, gameObject.transform.position, gameObject.transform.rotation);
+            mov = false;
+        }
         if (gameObject.transform.position.y % 10 == 0)
         {
-
+            mov = true;
         }
 	}
 }
