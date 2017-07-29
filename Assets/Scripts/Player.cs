6605ac673cdc7c3fa1e781e1bed0d96004f6eaea
Unknown <manumeq@gmail.com>
2017-07-29 09:31:07 +0200

    pilas,spawn_point y mas scripts

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,22 @@
 ﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
 
     public string currentColor;
     public float jumpForce = 10f;
+
+    private bool start;
+
+
+    //variables para el control de la hp bar y la hp
+    public float hp = 100f;
+    public float maxHP = 100f;
+    public float minHP = 0f;
+    public Slider hpBar;
+    private float decreaseSpeed = 1f;
 
     public SpriteRenderer sprenderer;
     public Rigidbody2D rigidBody;
@@ -14,6 +26,9 @@ public class Player : MonoBehaviour
 
     void Start()
     {
+        hpBar.value = hp;
+        start = false;
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
         /*  ---ALTERNATE WAY OF DEFINING COLORS PRIVATELY
         colors = new Color[4];
         colors[0] = new Color(255, 255, 255);
@@ -27,9 +42,31 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //comenzamos a perder energia cuando empieza el juego
+        if (start) { 
+            hp = hp - (decreaseSpeed * Time.deltaTime);
+            decreaseSpeed += (1f * Time.deltaTime);
+
+            hpBar.value = hp;
+            if (hp <= 0)
+            {
+                killPlayer();
+            }
+        }
+
         if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
         {
-            rigidBody.velocity = Vector2.up * jumpForce * Time.deltaTime;
+            //al empezar se pone la gravedad a 0 hasta la primera ejecucion del update donde se pone su valor normal
+            //esto evita que pasen cosas raras al empezar
+            if (start)
+            {
+                rigidBody.velocity = Vector2.up * jumpForce * Time.deltaTime;
+            }
+            else
+            {
+                gameObject.GetComponent<Rigidbody2D>().gravityScale = 3;
+                start = true;
+            }
         }
 
     }
@@ -45,12 +82,16 @@ public class Player : MonoBehaviour
 
         if (collision.tag!=currentColor)
         {
-            deathPlayer.GetComponent<ParticleSystem>().startColor = gameObject.GetComponent<SpriteRenderer>().color;
-            Instantiate(deathPlayer, transform.position, transform.rotation);
-            Destroy(gameObject);
-
+            killPlayer();
         }
 
+    }
+
+    void killPlayer()
+    {
+        //deathPlayer.GetComponent<ParticleSystem>().startColor = gameObject.GetComponent<SpriteRenderer>().color;
+        Instantiate(deathPlayer, transform.position, transform.rotation);
+        Destroy(gameObject);
     }
 
     void setRandomColor()
---
+++ Assets/Scripts/spawnPoint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnPoint : MonoBehaviour {
+    public int creationSpeed;
+    private float tiempo;
+    public GameObject barraPositiva;
+    public GameObject barraNegativa;
+	// Use this for initialization
+	void Start () {
+        tiempo = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tiempo -= creationSpeed * Time.deltaTime;
+        if (tiempo < 0)
+        {
+            tiempo = 5;
+            Instantiate(barraPositiva, gameObject.transform.position, gameObject.transform.rotation);
+        }
+	}
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
